aac7dcb8e7a3b854a82c7427e2691427a12c8299
Kevin-Kawai <kevin.fredricks@gmail.com>
2019-03-03 21:18:31 +0900

    added print puzzle and print puzzle solution to challenge runner

--- bin/amidakuji.rb
+++ bin/amidakuji.rb
@@ -9,17 +9,48 @@ puzzle = puzzles[puzzle_number]
 
 puts "working on puzzle #{puzzle_number}"
 
-goal_line = Amidakuji.solve(
-  horizontal_space_count: puzzle.horizontal_space_count,
-  vertical_space_count: puzzle.vertical_space_count,
-  horizontal_lines: puzzle.horizontal_lines,
-  start_location: puzzle.start_location
-)
+case ARGV[0]
+when "--print_puzzle"
+  puzzle_output = Amidakuji.print_puzzle(
+    horizontal_space_count: puzzle.horizontal_space_count,
+    vertical_space_count: puzzle.vertical_space_count,
+    horizontal_lines: puzzle.horizontal_lines,
+    start_location: puzzle.start_location
+  )
 
-if goal_line == puzzle.solution
-  puts "that is the correct answer!"
+  puts puzzle_output
+
+  if puzzle_output == puzzle.board
+    puts "that is the correct answer!"
+  else
+    puts "the board does not look correct"
+  end
+when "--print_puzzle_solution"
+  puzzle_output = Amidakuji.print_puzzle_solution(
+    horizontal_space_count: puzzle.horizontal_space_count,
+    vertical_space_count: puzzle.vertical_space_count,
+    horizontal_lines: puzzle.horizontal_lines,
+    start_location: puzzle.start_location
+  )
+
+  puts puzzle_output
+
+  if puzzle_output == puzzle.boardWithSolution
+    puts "that is the correct answer!"
+  else
+    puts "the board does not look correct"
+  end
 else
-  puts "you returned #{goal_line}, but that is incorrect"
-end
+  goal_line = Amidakuji.solve(
+    horizontal_space_count: puzzle.horizontal_space_count,
+    vertical_space_count: puzzle.vertical_space_count,
+    horizontal_lines: puzzle.horizontal_lines,
+    start_location: puzzle.start_location
+  )
 
-# TODO: add options for print_puzzle and print_puzzle_solution
+  if goal_line == puzzle.solution
+    puts "that is the correct answer!"
+  else
+    puts "you returned #{goal_line}, but that is incorrect"
+  end
+end
