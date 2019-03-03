2c76b543644fa218296702733fc6203cefbc1c70
Kevin-Kawai <kevin.fredricks@gmail.com>
2019-03-03 21:18:07 +0900

    changed data format to YAML

--- lib/amidakuji/puzzle_reader.rb
+++ lib/amidakuji/puzzle_reader.rb
@@ -1,10 +1,10 @@
-require "json"
+require "yaml"
 
 class Amidakuji::PuzzleReader
   attr_reader :puzzles
 
   class PuzzleData
-    attr_reader :horizontal_space_count, :vertical_space_count, :horizontal_lines, :start_location, :solution
+    attr_reader :horizontal_space_count, :vertical_space_count, :horizontal_lines, :start_location, :solution, :board, :boardWithSolution
 
     def initialize(data)
       @horizontal_space_count = data["horizontalSpaceCount"]
@@ -12,12 +12,14 @@ class Amidakuji::PuzzleReader
       @horizontal_lines = data["horizontalLines"]
       @start_location = data["startLocation"]
       @solution = data["solution"]
+      @board = data["board"]
+      @boardWithSolution = data["boardWithSolution"]
     end
   end
 
   def initialize
-    json_file = File.expand_path("../../puzzles/puzzle_data.json", __dir__)
-    data = JSON.parse(File.read(json_file))
+    yaml_file = File.expand_path("../../puzzles/puzzle_data.yml", __dir__)
+    data = YAML.load_file(yaml_file)
     @puzzles = data.map {|puzzle_data| PuzzleData.new(puzzle_data)}
   end
 end
--- test/amidakuji_test.rb
+++ test/amidakuji_test.rb
@@ -5,18 +5,39 @@ require_relative "../lib/amidakuji/puzzle_reader"
 
 
 class AmidakujiTest < Minitest::Test
-  puzzles = Amidakuji::PuzzleReader.new.puzzles
-  puzzles.each.with_index do |puzzle, i|
-    define_method "test_solve_#{i}" do
-      assert_equal(puzzle.solution,
-        Amidakuji.solve(
-          horizontal_space_count: puzzle.horizontal_space_count,
-          vertical_space_count: puzzle.vertical_space_count,
-          horizontal_lines: puzzle.horizontal_lines,
-          start_location: puzzle.start_location,
-        )
+  def test_solve
+    puzzle = PuzzleReader.new
+    assert_equal(puzzle.solution,
+      Amidakuji.solve(
+        horizontal_space_count: puzzle.horizontal_space_count,
+        vertical_space_count: puzzle.vertical_space_count,
+        horizontal_lines: puzzle.horizontal_lines,
+        start_location: puzzle.start_location,
       )
-    end
+    )
   end
 
+  def test_print_puzzle
+    puzzle = PuzzleReader.new
+    assert_equal(puzzle.solution,
+      Amidakuji.print_puzzle(
+        horizontal_space_count: puzzle.horizontal_space_count,
+        vertical_space_count: puzzle.vertical_space_count,
+        horizontal_lines: puzzle.horizontal_lines,
+        start_location: puzzle.start_location,
+      )
+    )
+  end
+
+  def test_print_puzzle_solution
+    puzzle = PuzzleReader.new
+    assert_equal(puzzle.solution,
+      Amidakuji.print_puzzle_solution(
+        horizontal_space_count: puzzle.horizontal_space_count,
+        vertical_space_count: puzzle.vertical_space_count,
+        horizontal_lines: puzzle.horizontal_lines,
+        start_location: puzzle.start_location,
+      )
+    )
+  end
 end
