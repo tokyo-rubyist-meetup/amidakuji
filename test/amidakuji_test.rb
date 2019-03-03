b874c3e3f8b0640d30699399bed62a973cbddd9c
Paul McMahon <paul@doorkeeper.jp>
2019-03-04 08:21:55 +0900

    Tests for printing puzzle / solution

--- test/amidakuji_test.rb
+++ test/amidakuji_test.rb
@@ -17,6 +17,28 @@ class AmidakujiTest < Minitest::Test
         )
       )
     end
+
+    define_method "test_print_puzzle_#{puzzle.number}" do
+      assert_equal(puzzle.board,
+        Amidakuji.print_puzzle(
+          horizontal_space_count: puzzle.horizontal_space_count,
+          vertical_space_count: puzzle.vertical_space_count,
+          horizontal_lines: puzzle.horizontal_lines,
+          start_location: puzzle.start_location,
+        )
+      )
+    end
+
+    define_method "test_print_puzzle_solution_#{puzzle.number}" do
+      assert_equal(puzzle.board_with_solution,
+        Amidakuji.print_puzzle_solution(
+          horizontal_space_count: puzzle.horizontal_space_count,
+          vertical_space_count: puzzle.vertical_space_count,
+          horizontal_lines: puzzle.horizontal_lines,
+          start_location: puzzle.start_location,
+        )
+      )
+    end
   end
 
 end
