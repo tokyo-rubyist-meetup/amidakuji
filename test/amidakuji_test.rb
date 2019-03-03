ef0e70e9cc3992454eaf385c34593b9990cd18fd
Paul McMahon <paul@doorkeeper.jp>
2019-03-03 10:27:12 +0900

    Remove test of non-solve methods, as they create noise for beginners

--- test/amidakuji_test.rb
+++ test/amidakuji_test.rb
@@ -16,28 +16,4 @@ class AmidakujiTest < Minitest::Test
       )
     )
   end
-
-  def test_print_puzzle
-    puzzle = PuzzleReader.new
-    assert_equal(puzzle.solution,
-      Amidakuji.print_puzzle(
-        horizontal_space_count: puzzle.horizontal_space_count,
-        vertical_space_count: puzzle.vertical_space_count,
-        horizontal_lines: puzzle.horizontal_lines,
-        start_location: puzzle.start_location,
-      )
-    )
-  end
-
-  def test_print_puzzle_solution
-    puzzle = PuzzleReader.new
-    assert_equal(puzzle.solution,
-      Amidakuji.print_puzzle_solution(
-        horizontal_space_count: puzzle.horizontal_space_count,
-        vertical_space_count: puzzle.vertical_space_count,
-        horizontal_lines: puzzle.horizontal_lines,
-        start_location: puzzle.start_location,
-      )
-    )
-  end
 end
