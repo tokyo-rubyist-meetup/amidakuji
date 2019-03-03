3296424be66212ddb61516f004b50d03ab9893b0
Paul McMahon <paul@doorkeeper.jp>
2019-03-03 10:45:28 +0900

    Test against all of the sample puzzles

--- test/amidakuji_test.rb
+++ test/amidakuji_test.rb
@@ -5,17 +5,18 @@ require_relative "../lib/amidakuji/puzzle_reader"
 
 
 class AmidakujiTest < Minitest::Test
-  def test_solve
-    puzzles = Amidakuji::PuzzleReader.new.puzzles
-    puzzle_number = rand(puzzles.length)
-    puzzle = puzzles[puzzle_number]
-    assert_equal(puzzle.solution,
-      Amidakuji.solve(
-        horizontal_space_count: puzzle.horizontal_space_count,
-        vertical_space_count: puzzle.vertical_space_count,
-        horizontal_lines: puzzle.horizontal_lines,
-        start_location: puzzle.start_location,
+  puzzles = Amidakuji::PuzzleReader.new.puzzles
+  puzzles.each.with_index do |puzzle, i|
+    define_method "test_solve_#{i}" do
+      assert_equal(puzzle.solution,
+        Amidakuji.solve(
+          horizontal_space_count: puzzle.horizontal_space_count,
+          vertical_space_count: puzzle.vertical_space_count,
+          horizontal_lines: puzzle.horizontal_lines,
+          start_location: puzzle.start_location,
+        )
       )
-    )
+    end
   end
+
 end
